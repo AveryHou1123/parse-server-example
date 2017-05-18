a0e5468ba3b7ff135927faf88cce8d3cf26ca1cf
AveryHou1123 <avery.hou@gmail.com>
2017-05-18 15:35:37 +0800

    update hb.parse-conn.js

--- public/js/hb.parse-conn.js
+++ public/js/hb.parse-conn.js
@@ -2,5 +2,7 @@
 /** prod **/
 var appid = "5cd282b1-127a-4e5b-b783-728897232f21";
 var jskey = "40f69cee-32d0-4f76-9da6-4e468a8b2be4";
-Parse.initialize(appid, jskey);
+//Parse.initialize(appid, jskey);
 
+Parse.initialize(appid);
+Parse.serverURL = 'http://go94igo.herokuapp.com/parse';
